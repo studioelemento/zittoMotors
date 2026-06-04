becf2fb2e2f42f31868800fda4db1054a19233b7
Jassy Mon <jassymon114@gmail.com>
2026-06-04 11:13:06 +0530

    added product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import HomePage from './page/landing/home/HomePage'
+import React from "react";
+import HomePage from "./page/landing/home/HomePage";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import ProductPage from "./page/products/ProductPage";
 
 const App = () => {
   return (
-    <div><HomePage/></div>
-  )
-}
+    <div>
+      {" "}
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductPage />} />
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
